2c10b87de4a7beb7e2be2b4a475d965ceb04b76c
Denis Kutsenko <deniskutchenko008@gmail.com>
2019-04-14 00:53:05 +0200

    new order fragment

--- app/src/main/java/ng/com/obkm/bottomnavviewwithfragments/DashboardFragment.java
+++ app/src/main/java/ng/com/obkm/bottomnavviewwithfragments/DashboardFragment.java
@@ -10,13 +10,10 @@ import android.widget.Button;
 import android.widget.TextView;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
+
 public class DashboardFragment extends Fragment {
 
-    public TextView countTv;
-    public Button countBtn;
+
 
     public DashboardFragment() {
         // Required empty public constructor
@@ -26,22 +23,10 @@ public class DashboardFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        countTv = (TextView) view.findViewById(R.id.count_tv);
-        countTv.setText("0");
-        countBtn = (Button) view.findViewById(R.id.count_btn);
-        countBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                increaseCount();
-            }
-        });
+
         return view;
     }
 
-    private void increaseCount() {
-        int current = Integer.parseInt((String) countTv.getText());
-        countTv.setText(String.valueOf(current+1));
 
-    }
 
 }
